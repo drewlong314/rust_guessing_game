df8b5da0aaaac9c67f8bf1f6f92e4346f8d2ce65
drewlong314 <drewlong314@gmail.com>
2023-02-15 15:04:29 -0600

    get random number

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use std::io;
 
+use rand::Rng;
+
 fn main() {
+
     println!("Guess the number!");
 
     println!("Please input your guess.");
 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
     let mut guess = String::new();
+
+    println!("Secret number: {secret_number}");
 
     io::stdin()
         .read_line(&mut guess)
