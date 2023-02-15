f5adb528653897c2da534159384a80304e09f8c8
drewlong314 <drewlong314@gmail.com>
2023-02-15 15:13:24 -0600

    evaluating guess

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{io, cmp::Ordering};
 
 use rand::Rng;
 
@@ -16,6 +16,14 @@ fn main() {
     io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal=> println!("You win!"),
+    }
 
     println!("You guessed: {guess}");
 }
