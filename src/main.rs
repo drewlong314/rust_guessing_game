98801548c3417be656f78b69fbd847f6de2f2cf0
drewlong314 <drewlong314@gmail.com>
2023-02-15 15:24:10 -0600

    add loop to until right answer is guessed

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,33 @@
-use std::{io, cmp::Ordering};
+use std::{cmp::Ordering, io};
 
 use rand::Rng;
 
 fn main() {
-
     println!("Guess the number!");
 
-    println!("Please input your guess.");
-
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    let mut guess = String::new();
 
-    println!("Secret number: {secret_number}");
+    loop {
+        println!("Please input your guess.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal=> println!("You win!"),
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
-
-    println!("You guessed: {guess}");
 }
